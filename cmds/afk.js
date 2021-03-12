b31b412ea84254ee8dc33d0c15cf47f02b41a519
Hyoro-King <78290664+Hyoro-King@users.noreply.github.com>
2021-03-12 20:49:55 +0700

    Update afk.js

--- cmds/afk.js
+++ cmds/afk.js
@@ -5,14 +5,14 @@ module.exports.run = async (bot, message, args) => {
 
     
            const no1 = new Discord.MessageEmbed()
-           .setDescription("You can't put links KID**")
+           .setDescription("Bạn không nên cho Hyoku biết URL khi dùng lệnh AFK**")
            .setColor("RED")
           let text = args.join(" ")
        if(text.includes("www") || text.includes("discord.gg") || text.includes("discordapp") || text.startsWith("https://")) return message.channel.send(no1)
 
       
         
-    const status = new db.table("AFKs");
+    const status = new db.table("AFK");
     let afk = await status.fetch(message.author.id);
     const embed = new Discord.MessageEmbed()
     .setColor("GREEN")
@@ -26,7 +26,7 @@ module.exports.run = async (bot, message, args) => {
 
     
     if (!afk) {
-      embed.setDescription(`**__${message.author.tag}__ you are now AFK\nReason: ${text}**`)
+      embed.setDescription(`**__${message.author.tag}__ giờ đang bận:\nHiện __${message.author.tag}__ đang: ${text}**`)
       status.set(`${message.author.id}_${message.guild.id}`, text)
 
     message.channel.send(embed)  
